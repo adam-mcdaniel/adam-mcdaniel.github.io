d12de3cd1ccf840ed95512bc71eb7516cb250794
adam-mcdaniel <adam.mcdaniel17@gmail.com>
2020-08-26 19:53:59 -0400

    Fix link size

--- docs/sw.js
+++ docs/sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-a061991167425147f006.js"
+    "url": "webpack-runtime-62fc9347634546bcbb42.js"
   },
   {
     "url": "framework-fd2b85530eec85104a39.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "849b69fefba94fcc2edaab1b3054f950"
+    "revision": "707271ad3249a35013746c903e61fe29"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-0097d26fbd474b34ff9b.js"
